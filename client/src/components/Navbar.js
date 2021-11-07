d2b519a8e889ed36b8db49b8823f61cdbbc09169
catupperman <catupperman@gmail.com>
2021-11-07 01:10:00 -0400

    package json stuff

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+
+const AppNavbar = () => {
+    // set modal display state
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+        Hello, World?
+        </>
+    );
+};
+
+export default AppNavbar;
