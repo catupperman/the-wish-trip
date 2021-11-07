1a61bf7c3619a68b71eec40dafefb4cea98826a6
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-07 15:21:19 -0500

    adding dotenv config to index js, adding API testing code to API.js and APP.js

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -1,3 +1,20 @@
 import { useState, useEffect } from 'react';
 
 import search from '../utils/API';
+
+const Flights = () => {
+  // // Declare a new state variable "results"
+  // const [results, setResults] = useState([]);
+
+  // // Method to get search results and set state
+  // const searchFlights = async () => {
+  //   const response = await search();
+
+  // }
+  return (
+    <div>
+      search
+    </div>
+  )
+
+}
