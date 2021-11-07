722c47c2f163df2f0c48d1b5e3662a1b5ee58d9a
catupperman <catupperman@gmail.com>
2021-11-07 14:03:19 -0500

    scaredy cat

---
+++ client/src/components/Flights.js
@@ -0,0 +1,3 @@
+import { useState, useEffect } from 'react';
+
+import search from '../utils/API';
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,8 @@
-export const searchFlights = (query) => {
-   return fetch('http://')
-}
+import axios from 'axios';
+​
+const { REACT_APP_BASEURL, REACT_APP_APIKEY } = process.env;
+
+const search = (query) =>
+  axios.get(`${REACT_APP_BASEURL}${query}${REACT_APP_APIKEY}&rating=pg`);
+
+  export default search;
