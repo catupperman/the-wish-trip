7d075822d36fd4bb3ff0b5ce044f99aa85047ee8
catupperman <catupperman@gmail.com>
2021-11-08 21:55:46 -0500

    attempting to manipulate this thing

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -1,25 +1,51 @@
 import { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
-import SearchFlights from '../utils/API';
+import Auth from '../utils/auth';
+import { saveTrip, SearchFlights } from '../utils/API';
+
 
 const Flights = () => {
-  // // Declare a new state variable "results"
-   const [results, setResults] = useState([]);
 
-   const searchFlights = async (query) => {
-    const response = await SearchFlights(query);
-    setResults(response.data.data);
-  };
+    const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    searchFlights();
-  }, []);
-  return (
-    <div>
-      <SearchFlights results={results} />
-    </div>
-  )
+    const searchFlights = async (query) => {
+        const response = await SearchFlights(query);
+        setResults(response.data.data);
+    };
 
+    useEffect(() => {
+        searchFlights();
+    }, []);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!searchInput) {
+            return false;
+        }
+
+        try {
+            const response = await SearchFlights(searchInput);
+
+            if (!response.ok) {
+                throw new Error('something went wrong!');
+            }
+            const { items } = await response.json();
+
+            setSearchedFlights(flightData)
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <>
+            
+            <SearchFlights results={results} />
+        </>
+    )
 }
 
 export default Flights;
