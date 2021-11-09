f87488255d39818be068e69c185820ef6cb5f2bf
catupperman <catupperman@gmail.com>
2021-11-08 22:14:58 -0500

    adding notes befoe I lose my mind completely

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+//pretty sure I'm going to need this, ask John-Mark
 import Auth from '../utils/auth';
 import { saveTrip, SearchFlights } from '../utils/API';
 
-
+//Complies the build onto the page
 const Flights = () => {
     const [searchedFlights, setSearchedFlights] = useState([]);
     const [searchInput, setSearchInput] = useState('');
@@ -13,14 +13,14 @@ const Flights = () => {
         const response = await SearchFlights(query);
         setSearchedFlights(response.data.data);
     };
-
+    //allows for connection between api and react webpage
     useEffect(() => {
         searchFlights();
     }, []);
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-
+        //if there is no input, nothing happens on the screen, prevents default
         if (!searchInput) {
             return false;
         }
@@ -31,13 +31,15 @@ const Flights = () => {
             if (!response.ok) {
                 throw new Error('something went wrong!');
             }
+            //turns requests into json
             const { items } = await response.json();
-
+            //specific data from the API, and directed
+            //TODO: FIX THIS MANIPULATION MESS
             const flightData = items.map((flight) => ({
                 flightId: flight.id
 
             }))
-
+            //combines the data into a nice neat package?
             setSearchedFlights(flightData)
 
         } catch (err) {
@@ -47,31 +49,31 @@ const Flights = () => {
 
     return (
         <>
-           
-                <Contianer>
-                    <h1>Search for Flights!</h1>
-                    <Form onSubmit={handleFormSubmit}>
+
+            <Contianer>
+                <h1>Search for Flights!</h1>
+                <Form onSubmit={handleFormSubmit}>
                     <Form.Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name='searchInput'
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for Flights'
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='searchInput'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={4}>
+                            <Button type='submit' variant='success' size='lg'>
+                                Submit Search
                 </Button>
-              </Col>
-            </Form.Row>
-                    </Form>
-            <SearchFlights results={results} />
+                        </Col>
+                    </Form.Row>
+                </Form>
+                <SearchFlights results={results} />
             </Contianer>
-           
+
         </>
     )
 }
