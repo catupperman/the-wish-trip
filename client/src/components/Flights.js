3b2a4ace8d93f02c0abed985c91e6de65b16e73a
catupperman <catupperman@gmail.com>
2021-11-09 22:16:23 -0500

    flight api calls

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -12,8 +12,10 @@ const Flights = () => {
     const [flights, setFlights] = useState([]);
     const isEmpty = (arr) => !Array.isArray(arr) || arr.length === 0;
 
+    //results[0].id
+
     // useEffect(() => {
-    //     fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=Dublin', {
+    //     fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=${travelingTo}', {
     //         'headers': {
     //           'sec-fetch-dest': 'empty',
     //           'sec-fetch-mode': 'cors',
@@ -47,6 +49,69 @@ const Flights = () => {
     //         )
     // }, [])
 
+    //fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=${travelingFrom}', {
+    //         'headers': {
+    //           'sec-fetch-dest': 'empty',
+    //           'sec-fetch-mode': 'cors',
+    //           'sec-fetch-site': 'same-site',
+    //           'usequerystring': 'true',
+    //           'x-rapidapi-host': 'priceline-com-provider.p.rapidapi.com',
+    //           'x-rapidapi-key': 'ddd578e581msh5c1096c3181d1a0p1d59c8jsn253cf63240a9',
+    //           'x-rapidapi-ua': 'RapidAPI-Playground'
+    //         },
+    //         'referrer': 'https://rapidapi.com/',
+    //         'referrerPolicy': 'strict-origin-when-cross-origin',
+    //         'body': null,
+    //         'method': 'GET',
+    //         'mode': 'cors',
+    //         'credentials': 'omit'
+    //       }).then(res => res.json())
+    //         .then((result) => {
+    //             console.log("all good")
+    //             setIsLoaded(true);
+    //             setFlights(result);
+    //             setError(null);
+
+    //         })
+    //         .catch(error => {
+    //             console.log("all bad")
+    //             setIsLoaded(true);
+    //             setError(error);
+    //             setFlights([]);
+    //         }
+    //         )
+    // }, [])
+    //let locationDeparture = results[0].id
+    //let locationArrival = results[0].id
+    //     fetch("https://priceline-com-provider.p.rapidapi.com/v1/flights/search?sort_order=PRICE&location_departure=${locationDeparture}&date_departure=${departureDate}&class_type=ECO&location_arrival=${locationArrival}&itinerary_type=ONE_WAY&price_max=${maxTicketPrice}&date_departure_return=${returnDate}&duration_max=2051&number_of_stops=0&price_min=100&number_of_passengers=${passengers}", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "priceline-com-provider.p.rapidapi.com",
+    //         "x-rapidapi-key": "ddd578e581msh5c1096c3181d1a0p1d59c8jsn253cf63240a9"
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // }).then(res => res.json())
+    //         .then((result) => {
+    //             console.log("all good")
+    //             setIsLoaded(true);
+    //             setFlights(result);
+    //             setError(null);
+
+    //         })
+    //         .catch(error => {
+    //             console.log("all bad")
+    //             setIsLoaded(true);
+    //             setError(error);
+    //             setFlights([]);
+    //         }
+    //         )
+    // }, []);
+
     if (error) {
         return <div> Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -55,24 +120,63 @@ const Flights = () => {
         return <div>"no flights"</div>
     } else {
         return (
-            <ul>
-                {flights.map(flights => (
-                    <li key={flights.id}>
-                        {flights.name}
-                    </li>
-                ))}
-            </ul>
-        )
-    }
-}
-
-
-{/* <h1>Search for Flights!</h1>
+            <Container>
+                <h1>Search for Flights!</h1>
                 <Form onSubmit={handleFormSubmit}>
                     <Form.Row>
                         <Col xs={12} md={8}>
                             <Form.Control
-                                name='searchInput'
+                                name='travelingFrom'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='travelingTo'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='departureDate'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='returnDate'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='passengers'
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type='text'
+                                size='lg'
+                                placeholder='Search for Flights'
+                            />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                                name='maxTicketPrice'
                                 value={searchInput}
                                 onChange={(e) => setSearchInput(e.target.value)}
                                 type='text'
@@ -83,15 +187,21 @@ const Flights = () => {
                         <Col xs={12} md={4}>
                             <Button type='submit' variant='success' size='lg'>
                                 Submit Search
-                </Button>
+            </Button>
                         </Col>
                     </Form.Row>
-                </Form> */}
-{/* <SearchFlights />
-            
+                </Form>
+                <ul>
+                    {flights.map(flights => (
+                        <li key={flights.id}>
+                            {flights.name}
+                        </li>
+                    ))}
+                </ul>
+            </Container>
+        )
+    }
+}
 
-        </> */}
-//     )
-// }
 
 export default Flights;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,25 +6,31 @@ const AppNavbar = () => {
     // set modal display state
     // const [showModal, setShowModal] = useState(false);
     return (
-       <ul className="nav nav-tabs">
-           <li className="nav-item">
-           {/* <a
+        <>
+            <Navbar>
+                <Conatiner>
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            {/* <a
                 href="#ContactUs"
                 onClick={() => handlePageChange('ContactUs')}
                 className={'ContactUs'}
                 >
                     Contact Us
                 </a> */}
-                <button>Contact Us</button>
-           </li>
-           <li className="logIn">
-               <button>Login In</button>
-           </li>
-           <li className="signUp">
-               <button> Sign Up </button>
-           </li>
-           {/* These Two will eventually toggle away as a User has signed in or signed up to show "My Trips" as a button instead */}
-       </ul>
+                            <button>Contact Us</button>
+                        </li>
+                        <li className="logIn">
+                            <button>Login In</button>
+                        </li>
+                        <li className="signUp">
+                            <button> Sign Up </button>
+                        </li>
+                        {/* These Two will eventually toggle away as a User has signed in or signed up to show "My Trips" as a button instead */}
+                    </ul>
+                </Conatiner>
+            </Navbar>
+        </>
     );
 };
 
