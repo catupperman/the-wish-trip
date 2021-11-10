bf1811b04df60e693579e910decc33dd4027ce49
catupperman <catupperman@gmail.com>
2021-11-09 19:14:53 -0500

    deleted extra code I dont need

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -64,50 +64,7 @@ const Flights = () => {
             </ul>
         )
     }
-}    // const [searchedFlights, setSearchedFlights] = useState([]);
-// const [searchInput, setSearchInput] = useState('');
-
-// const searchFlights = async (query) => {
-//     const response = await SearchFlights(query);
-//     setSearchedFlights(response.data.data);
-// };
-// //allows for connection between api and react webpage
-// useEffect(() => {
-//     searchFlights();
-// }, []);
-
-// const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     //if there is no input, nothing happens on the screen, prevents default
-//     if (!searchInput) {
-//         return false;
-//     }
-
-//     try {
-//         const response = await SearchFlights(searchInput);
-
-//         if (!response.ok) {
-//             throw new Error('something went wrong!');
-//         }
-//         //turns requests into json
-//         const { items } = await response.json();
-//         //specific data from the API, and directed
-//         //TODO: FIX THIS MANIPULATION MESS
-//         const flightData = items.map((flight) => ({
-//             flightId: flight.id
-
-
-//         }))
-//         //combines the data into a nice neat package?
-//         setSearchedFlights(flightData)
-
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
-
-// return (
-//     <>
+}
 
 
 {/* <h1>Search for Flights!</h1>
