e2b7c25015804803871c00a20a048f057138fa11
catupperman <catupperman@gmail.com>
2021-11-11 00:53:52 -0500

    render page content

--- client/src/components/Landing_V2.js
+++ client/src/components/Landing_V2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Landing_V2.css';
+import ComponentChange from '../ComponentContainer';
 
 export default function LandingV2() {
   return (
@@ -8,8 +9,8 @@ export default function LandingV2() {
       <div className="hero-area">
         <img className="hero-image" src="https://images.unsplash.com/photo-1534951474654-87823058c487?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1123&q=80"></img>
         <div className="landing-button-group d-flex">
-          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signup-button">Sign Up</button>
-          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signin-button">Sign In</button>
+          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signup-button" href="#SignUpForm" onClick={() => ComponentChange('SignUpForm')} className={currentPage === 'SignUpForm' ? 'nav-link active' : 'nav-link'}>Sign Me Up</button>
+          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signin-button" href="#LoginForm" onClick={() => ComponentChange('LoginForm')} className={currentPage === 'LoginForm' ? 'nav-link active' : 'nav-link'}>I know the Drill, Sign Me In</button>
         </div>
       </div>
     </>
