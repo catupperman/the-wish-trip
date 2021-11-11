90380303b04ba3a8d49437a252eaeaaccc1a2a15
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-10 19:47:35 -0500

    adding Footer.js skeleton

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,30 @@ import { ModalFooter } from 'react-bootstrap';
 export default function Footer() {
   return (
     <>
-      <div>
-        Hello there
-      </div>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h5>
+                The Wish Trip
+              </h5>
+              <p>
+                Let us help you turn your wish into a trip!
+              </p>
+            </div>
+            <div className="col">
+              <h5>
+                Contact Us!
+              </h5>
+              <ul>
+                <li>Cat</li>
+                <li>John-Mark</li>
+                <li>Kira</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 } 
