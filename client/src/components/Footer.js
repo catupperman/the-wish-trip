9a6140a9e58849c36f2559765d53a636d99ce900
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-10 19:57:03 -0500

    adding team footer icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ModalFooter } from 'react-bootstrap';
+import '../styles/Footer.css';
 
 export default function Footer() {
   return (
     <>
+
+      {/* footer */}
       <footer>
         <div className="container">
           <div className="row">
+
+            {/* left side */}
             <div className="col">
               <h5>
                 The Wish Trip
@@ -15,14 +20,23 @@ export default function Footer() {
                 Let us help you turn your wish into a trip!
               </p>
             </div>
+
+            {/* right side */}
             <div className="col">
               <h5>
                 Contact Us!
               </h5>
-              <ul>
-                <li>Cat</li>
-                <li>John-Mark</li>
-                <li>Kira</li>
+              <ul className="icon-set">
+                
+                {/* Cat */}
+                <li><a href="https://github.com/catupperman" target="_blank"><img src="https://avatars.githubusercontent.com/u/84750356?v=4" alt="catupperman" title="Cat"/></a></li>
+
+                {/* John-Mark */}
+                <li><a href="https://github.com/Kalmnir" target="_blank"><img src="https://avatars.githubusercontent.com/u/84051952?v=4" alt="Kalmnir" title="John-Mark"/></a></li>
+
+                {/* Kira */}
+                <li><a href="https://github.com/j1741" target="_blank"><img src="https://avatars.githubusercontent.com/u/84889065?v=4" alt="J1741" title="Kira"/></a></li>
+
               </ul>
             </div>
           </div>
