3b41263ea953b2aeaa5b93b7c25211b7e850a57b
catupperman <catupperman@gmail.com>
2021-11-23 19:19:41 -0500

    FIXED IT

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,7 +12,7 @@ const Header = ({ currentPage, handlePageChange }) => {
         <div className="container-fluid d-flex">
           <a className="navbar-brand brand-text" href="#">The Wish Trip</a>
           <button className="btn btn-sm btn-outline-light bg-transparent shadow-sm logout-button" href="#">Log Out</button>
-          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signup-button" href="#SignUpForm" onClick={() => handlePageChange('SignUpForm')}>Sign Me Up</button>
+          <button className="btn btn-outline-light bg-transparent shadow-sm landing-signup-button" href="#SignupForm" onClick={() => handlePageChange('SignupForm')}>Sign Me Up</button>
           <button className="btn btn-outline-light bg-transparent shadow-sm landing-signin-button" href="#LoginForm" onClick={() => handlePageChange('LoginForm')}>Sign Me In</button>
         </div>
       </nav>
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/SignupForm.css'
+import '../styles/SignupForm.css';
 
 import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
